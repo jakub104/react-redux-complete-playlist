8a78a1a2c3601546825b77a139b478787bfc9cd4
jakub104 <jakubzlezena@wp.pl>
2019-08-02 12:54:33 +0200

    lesson-14 complete

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,16 +2,22 @@ import React, {Component} from 'react';
 import Ninjas from "./Ninjas"
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>My first React App!</h1>
-        <p>Welcome :o</p>
-        <Ninjas name="Jakub" age="17" belt="transparent" />
-        <Ninjas name="Kuba" age="18" belt="black" />
-      </div>
-    )
-  }
+	state = {
+		ninjas: [
+			{name: "Jakub", age: "17", belt: "transparent", id: 1},
+			{name: "Sabina", age: "45", belt: "transparent", id: 2},
+			{name: "Paulina", age: "21", belt: "transparent", id: 3}
+		]
+	}
+	render() {
+		return (
+			<div className="App">
+				<h1>My first React App!</h1>
+				<p>Welcome :o</p>
+				<Ninjas ninjas={this.state.ninjas} />
+			</div>
+		)
+	}
 }
 
 export default App;
